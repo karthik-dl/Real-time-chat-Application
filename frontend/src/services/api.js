7ac211c1237c97b91fe917d5cd74328201328c5f
karthik-dl <dlkarthik462@gmail.com>
2026-01-06 22:51:26 +0530

    api changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://real-time-chat-application-gamg.onrender.com"
 });
 
 api.interceptors.request.use((config) => {
--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000", {
+export const socket = io("https://real-time-chat-application-gamg.onrender.com", {
   withCredentials: true,
   autoConnect: true,
 });
